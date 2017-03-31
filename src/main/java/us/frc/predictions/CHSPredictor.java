627be2582d26b773c95a4ecd89372cdd00efdbde
Jesse Knight <Jesse Knight@JAKLOCAL.macedon.local>
2017-03-31 13:33:41 -0400

    Refactored schedule to a generator class

--- src/main/java/us/frc/predictions/CHSPredictor.java
+++ src/main/java/us/frc/predictions/CHSPredictor.java
@@ -22,151 +22,14 @@ public class CHSPredictor {
   // TODO - JavaFX Config display
   private static final String EVENT_TO_PREDICT = "2017chcmp";
 
-  /*
-   * NOTE NOTE NOTE NOTE NOTE NOTE NOTE
-   * 
-   * SCHEDULE.length must be appropriate for the # of teams attending the event,
-   * as retrieved by TBA! If this isn't the case, then re-generate
-   * a schedule for N teams with X matches (for CHCMP it's 58 teams @ 12 matches)
-   * 
-   * Ideally we'd have pre-calculated schedules for 24 - 100 teams in
-   * a file somewhere, but that would take about 10 hours to generate
-   * using MatchMaker.exe and then another few hours to get into a 
-   * parse-able format.
-   * 
-   * TODO - automate this
-   * Run MatchMaker.exe -t 58 -r 12 -b -s -u 3 > Schedule.txt
-   *  - Replace '58' with # of teams
-   *  - Replace '12' with # of matches
-   *  - Edit "Schedule.txt"
-   *  -- Delete all occurances of ' 0'
-   *  -- Add in quotes & commas (find/replace in Sublime Text makes this easy)
-   *  -- Copy result here
-   */
-  private static final String[] SCHEDULE = new String[]{
-    "51 39 38 5 50 37",
-    "34 28 53 10 12 22",
-    "6 19 35 40 26 47",
-    "18 20 58 55 4 54",
-    "46 15 13 32 57 45",
-    "43 9 21 16 56 48",
-    "31 30 36 25 42 24",
-    "17 7 1 52 33 11",
-    "49 27 29 14 3 44",
-    " 23 41 55 5 8 2",
-    "53 4 38 43 26 46",
-    "37 57 15 22 34 9",
-    "56 25 52 28 33 48",
-    "20 10 7 17 21 49",
-    "12 51 8 16 31 19",
-    "35 47 50 58 30 44",
-    "39 40 18 14 23 45",
-    "13 11 41 3 1 42",
-    "36 32 2 54 27 6",
-    "24 20 4 29 19 51",
-    "35 31 7 56 5 22",
-    "49 38 30 55 12 28",
-    "9 3 23 52 34 26",
-    "48 36 13 39 10 47",
-    "50 21 58 45 11 8",
-    "27 15 17 54 43 40",
-    "33 6 42 18 44 57",
-    "1 32 16 24 53 29",
-    "46 2 41 37 14 25",
-    "30 39 3 19 8 20",
-    "47 23 51 27 11 56",
-    "15 42 22 38 58 33",
-    "29 55 21 6 57 28",
-    "46 24 5 17 18 10",
-    "12 50 14 43 25 32",
-    "44 35 34 54 31 13",
-    "52 2 37 40 4 16",
-    "48 41 45 7 26 53",
-    "58 9 1 46 36 49",
-    "20 14 27 28 22 11",
-    "21 6 34 25 50 17",
-    "23 54 32 42 16 55",
-    "3 18 2 15 47 31",
-    "33 5 12 26 4 29",
-    "52 45 49 35 43 41",
-    "13 53 19 36 56 37",
-    "9 7 51 1 44 39",
-    "8 48 24 57 40 38",
-    "10 5 3 30 28 54",
-    "11 49 26 16 25 18",
-    "19 17 43 55 22 13",
-    "21 47 52 12 37 53",
-    "36 45 1 44 2 48",
-    "41 8 39 4 27 33",
-    "23 50 10 29 35 57",
-    "56 24 40 42 34 58",
-    "15 51 30 7 46 32",
-    "6 38 20 14 31 9",
-    "26 5 28 45 13 43",
-    "12 54 47 8 23 33",
-    "50 48 27 19 37 18",
-    "11 17 36 41 22 57",
-    "1 53 31 6 40 30",
-    "49 9 4 32 35 24",
-    "38 46 14 34 56 3",
-    "25 20 39 7 15 55",
-    "29 52 42 51 21 2",
-    "44 16 27 10 58 41",
-    "22 43 4 30 1 23",
-    "35 3 46 48 6 12",
-    "32 39 26 31 11 34",
-    "21 18 45 38 29 36",
-    "47 57 5 49 25 15",
-    "14 53 42 9 17 8",
-    "2 28 19 7 16 58",
-    "13 24 52 20 54 50",
-    "55 37 40 44 10 56",
-    "33 45 30 34 48 51",
-    "47 18 22 41 32 21",
-    "53 3 57 2 58 39",
-    "4 7 25 13 38 27",
-    "26 42 37 1 55 20",
-    "33 16 14 5 36 35",
-    "50 51 56 10 49 54",
-    "43 29 11 40 9 46",
-    "8 44 6 15 52 19",
-    "28 24 17 31 12 23",
-    "5 34 16 27 1 47",
-    "54 7 14 57 21 4",
-    "22 36 40 3 55 50",
-    "25 13 58 51 52 18",
-    "45 19 12 42 10 9",
-    "31 44 24 53 46 11",
-    "26 30 56 17 39 29",
-    "41 49 6 38 48 23",
-    "8 15 32 28 37 35",
-    "2 43 10 20 33 34",
-    "27 55 31 9 52 53",
-    "45 54 25 19 1 14",
-    "11 58 48 4 6 5",
-    "22 17 38 47 29 44",
-    "56 41 12 50 2 15",
-    "37 33 21 24 49 3",
-    "23 46 16 30 13 20",
-    "40 8 28 51 36 26",
-    "18 42 35 39 43 7",
-    "57 19 10 32 38 52",
-    "58 29 54 37 41 31",
-    "11 30 9 2 13 33",
-    "48 26 55 24 14 15",
-    "42 28 50 4 47 46",
-    "32 40 44 21 20 5",
-    "3 51 25 53 35 17",
-    "34 23 43 36 8 7",
-    "57 56 1 18 27 12",
-    "16 22 49 39 45 6"
-  };
 
   // TODO - JavaFX Config display
   private static final int NUM_SCHEDULES_TO_PREDICT = 10000;
   
   public static void main(String... args) {
     Map<Integer, TeamStat> allEventStats = new HashMap<>();
+    
+    String[] schedule = ScheduleGenerator.getScheduleForTeams(12, 58);
     
     for(String event : PRIOR_EVENTS) {
       System.out.println("Processing " + event);
@@ -213,7 +76,7 @@ public class CHSPredictor {
     // Time to predict!
     for(int i = 0; i < NUM_SCHEDULES_TO_PREDICT; i++) {
 //      System.out.println("Prediction schedule " + i);
-      SchedulePredictor sp = new SchedulePredictor(dcmpTeams, allEventStats, SCHEDULE);
+      SchedulePredictor sp = new SchedulePredictor(dcmpTeams, allEventStats, schedule);
       // TODO - JavaFX Match display
       for(SchedulePredictor.Match m : sp.getMatches()) {
         allsimscores += m.getTotalScore();
@@ -286,11 +149,11 @@ public class CHSPredictor {
       nf.format((double)SchedulePredictor.NUM_KPA_RP / (double)NUM_SCHEDULES_TO_PREDICT));
     System.out.println("# of Ties predicted: " +
       nf.format((double)SchedulePredictor.NUM_TIES / (double)NUM_SCHEDULES_TO_PREDICT));
-    System.out.println("Average Alliance Score: " + nf.format(allsimscores/(double)SCHEDULE.length/(double)NUM_SCHEDULES_TO_PREDICT/2d));
-    System.out.println("Average Winning Score: " + nf.format(allsimwinscores/(double)SCHEDULE.length/(double)NUM_SCHEDULES_TO_PREDICT));
+    System.out.println("Average Alliance Score: " + nf.format(allsimscores/(double)schedule.length/(double)NUM_SCHEDULES_TO_PREDICT/2d));
+    System.out.println("Average Winning Score: " + nf.format(allsimwinscores/(double)schedule.length/(double)NUM_SCHEDULES_TO_PREDICT));
     double avgtiebreak = countFuelTieBreakers/(double)NUM_SCHEDULES_TO_PREDICT;
     System.out.println("# of Matches Where Fuel broke the Tie: " + 
-      nf.format(avgtiebreak) + " (" + nf.format(avgtiebreak/(double)SCHEDULE.length*100d) + "%)");
+      nf.format(avgtiebreak) + " (" + nf.format(avgtiebreak/(double)schedule.length*100d) + "%)");
   }
   
   /**
